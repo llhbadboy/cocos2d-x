2cd82b9755107240df58b5a33500c8b8fc30161e
linhui <llhbadboy@gmail.com>
2013-09-25 09:55:27 +0800

    add plugin/publish

---
+++ plugin/tools/toolsForPublish/environment.sh
@@ -0,0 +1,4 @@
+export ANDROID_NDK_ROOT=/Users/Alex/AndroidDev/android-ndk-r8b
+export ANDROID_SDK_ROOT=/Users/Alex/AndroidDev/android-sdk-mac_x86
+export ANT_PATH=/usr/bin
+export INPUT_SDK_PATH=/Users/Alex/AndroidDev/android-sdk-mac_x86
